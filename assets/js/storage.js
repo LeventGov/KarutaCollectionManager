d030c8b4412725d15eac8f9261d8705318f98729
Levent Govaerts <levent.govaerts@student.howest.be>
2025-12-15 20:49:33 +0100

    Remove comments from codebase

--- assets/js/storage.js
+++ assets/js/storage.js
@@ -1,14 +1,8 @@
-// Local storage management for Karuta Collection
-
 class StorageManager {
   constructor(storageKey = 'karuta-collection') {
     this.storageKey = storageKey;
   }
 
-  /**
-   * Load collection from localStorage
-   * @returns {Array} Collection array or empty array if not found
-   */
   load() {
     try {
       const saved = localStorage.getItem(this.storageKey);
@@ -19,10 +13,6 @@ class StorageManager {
     }
   }
 
-  /**
-   * Save collection to localStorage
-   * @param {Array} collection - Collection array to save
-   */
   save(collection) {
     try {
       localStorage.setItem(this.storageKey, JSON.stringify(collection));
@@ -31,21 +21,13 @@ class StorageManager {
     }
   }
 
-  /**
-   * Clear all collection data
-   */
   clear() {
     localStorage.removeItem(this.storageKey);
   }
 
-  /**
-   * Check if collection exists in storage
-   * @returns {boolean}
-   */
   exists() {
     return localStorage.getItem(this.storageKey) !== null;
   }
 }
 
-// Create global instance
 const storage = new StorageManager();
